044e118835807966508c193acb2fd027aecfd4a2
alumno <alumno>
2019-02-04 22:11:29 -0300

    Ejercicios 1,2 y 3

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,9 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-
+{   var nombre;
+	nombre= PROMPT("Ingrese su nombre")
+	nombre= elNombre.value;
+	alert(nombre);
 }
 
