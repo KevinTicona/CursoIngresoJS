12b0641822af4f12bb51419d98a80746306bba6b
alumno <alumno>
2019-02-13 19:21:47 -0300

    Ejercicio 1 del TP

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,7 +1,7 @@
 
 function mostrar()
 {
-	var largo;
+	/*var largo;
 	var ancho;
 	var resultado;
 
@@ -13,6 +13,25 @@ function mostrar()
 
 	resultado = largo*2+ancho*2;
 
-	alert("El perimetro seria: "+resultado);
+	alert("El perimetro seria: "+resultado);*/
+
+	/*1_alert("ustedes son xxxx y xxxx, sus edades son xx y xx,y la suma de sus edades es xx")*/
 	
+	var primerNombre;
+	var segundoNombre
+	var primerEdad;
+	var segundaEdad;
+
+	primerNombre=prompt("Ingrese un Nombre");
+	segundoNombre= prompt("Ingrese otro Nombre");
+
+	primerEdad=prompt("Ingrese la edad del primer Nombre");
+	segundaEdad=prompt("Ingrese la edad del segundo Nombre");
+
+	primerEdad=parseInt(primerEdad);
+	segundaEdad=parseInt(segundaEdad);
+
+	suma =primerEdad+segundaEdad;
+
+	alert("Ustedes son "+primerNombre+" y "+segundoNombre+", sus edades son "+primerEdad+" y "+segundaEdad+",y la suma de sus edades es "+suma);
 }
