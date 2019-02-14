c882ebd6eddd5506219b2970e853130ccaf9ca02
alumno <alumno>
2019-02-13 21:51:37 -0300

    Ultima actualizacion

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+	/* al realizar una compra,si compra de mas de dos productos se realiza un descuento del 10% y si supera los $2000 se agrega 
+	un descuento adicional de 15%, si el pago s con tarjeta y no con efectivo el precio final se le agrega un 10% de recargo*/
 
+	var cantidadDeProductos;
+	var totalDeCompra;
+	var tipoDePago;
+	var resultado;
+	var recargo;
+	var resultadoFinal;
+
+	cantidadDeProductos=prompt("Ingrese el total de productos que compro.");
+	totalDeCompra=prompt("Ingrese el total de pago.");
+	tipoDePago=prompt("Si paga en efectivo ingrese 1 y si es en tarjeta 2");
+
+
+	cantidadDeProductos=parseInt(cantidadDeProductos);
+	totalDeCompra=parseInt(totalDeCompra);
+	tipoDePago=parseInt(tipoDePago);
+
+	
+
+	if(cantidadDeProductos>2){
+		resultado=totalDeCompra*10/100;
+		resultadoFinal=totalDeCompra-resultado;
+		if(totalDeCompra>2000){
+			resultado=totalDeCompra*25/100;
+			resultadoFinal=totalDeCompra-resultado;
+			if(tipoDePago=2){
+			recargo=resultadoFinal*10/100;
+			resultadoFinal=resultadoFinal+recargo;
+			}
+		}
+	}
+	alert("Su pago seria de "+resultadoFinal);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -8,7 +8,21 @@ function mostrar()
   
   alert("Usted es "+nombre+" y vive en la localidad de "+localidad);*/
 
+  /*alert("El precio del producto en efectivo es :$xxxx. con tarjeta tiene un recargo de 10% que seria $xx de recargo, costandole final $xxx")*/
   
-  
+  var pagoEfectivo;
+  var multiplicacion;
+  var division;
+  var costoFinal;
+
+  pagoEfectivo = prompt("Ingrese el precio del producto en efectivo");
+  pagoEfectivo= parseInt(pagoEfectivo);
+
+  multiplicacion=pagoEfectivo*10;
+  division=multiplicacion/100;
+
+  costoFinal= pagoEfectivo+division;
+
+  alert("El precio del producto en efectivo es: "+pagoEfectivo+" con tarjeta tiene un recargo de 10% que seria "+division+" de recargo, costandole final "+costoFinal);
 
 }
